00100d1c9b08905b7dc17fb10f8a7c0bad576eaf
sazzadtomal <sazzadtomalfordesktop1660@gmail.com>
2023-08-16 15:17:57 +0600

    added mutatuion queries using RTK Query

--- src/features/todos/TodoList.js
+++ src/features/todos/TodoList.js
@@ -2,15 +2,30 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrash, faUpload } from '@fortawesome/free-solid-svg-icons'
 import { useState } from "react"
+import { useGetTodosQuery,useAddTodoMutation,
+    useUpdateTodoMutation,
+    useDeleteTodoMutation } from '../api/apiSlice'
+
+
 
 const TodoList = () => {
     const [newTodo, setNewTodo] = useState('')
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        //addTodo
+        addTodo({userId:1,title:newTodo,completed: false})
         setNewTodo('')
     }
+
+    const [addTodo] = useAddTodoMutation()
+    const [updateTodo] = useUpdateTodoMutation()
+    const [deleteTodo] = useDeleteTodoMutation()
+
+    const {data:todos,isLoading,isSuccess,isError,error}=useGetTodosQuery()
+
+
+
+
 
     const newItemSection =
         <form onSubmit={handleSubmit}>
@@ -30,9 +45,31 @@ const TodoList = () => {
         </form>
 
 
-    let content;
-    // Define conditional content
-
+let content;
+if (isLoading) {
+    content = <p>Loading...</p>
+} else if (isSuccess) {
+    content = todos.map(todo => { //JSON.stringify(todos)
+        return (
+            <article key={todo.id}>
+                <div className="todo">
+                    <input
+                        type="checkbox"
+                        checked={todo.completed}
+                        id={todo.id}
+                        onChange={() => updateTodo({ ...todo, completed: !todo.completed })}
+                    />
+                    <label htmlFor={todo.id}>{todo.title}</label>
+                </div>
+                <button className="trash" onClick={() => deleteTodo({ id: todo.id })}>
+                    <FontAwesomeIcon icon={faTrash} />
+                </button>
+            </article>
+        )
+    })
+} else if (isError) {
+    content = <p>{error}</p>
+}
     return (
         <main>
             <h1>Todo List</h1>
--- src/index.js
+++ src/index.js
@@ -3,9 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import { ApiProvider } from '@reduxjs/toolkit/dist/query/react';
+import { apiSlice } from './features/api/apiSlice';
+
+
+
 ReactDOM.createRoot(document.getElementById('root'))
   .render(
     <React.StrictMode>
-      <App />
+      <ApiProvider api={apiSlice}>
+        <App />
+      </ApiProvider>
     </React.StrictMode>
   );
